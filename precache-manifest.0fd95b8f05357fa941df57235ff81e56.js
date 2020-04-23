5a4605c57a2e36faadbc25572bea4e0fadcc0ce0
Wei Zhang <iusenenow@gmail.com>
2020-04-23 12:23:31 +1000

    Updates

---
+++ precache-manifest.0fd95b8f05357fa941df57235ff81e56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4845a5a2ec50d12e299a73f99144fc80",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "8f23bc4ff2ba8402d3c5",
+    "url": "/covid-19/static/css/main.02392804.chunk.css"
+  },
+  {
+    "revision": "56bb8ed11cba6b845e42",
+    "url": "/covid-19/static/js/2.2daa9433.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.2daa9433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f23bc4ff2ba8402d3c5",
+    "url": "/covid-19/static/js/main.b00bea52.chunk.js"
+  },
+  {
+    "revision": "187d883a0240292b3ee8",
+    "url": "/covid-19/static/js/runtime-main.adeadfc8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
